289258c0e28d7d7eea1089ce03de30de92058c60
afsaanchez04 <afsanchez04@gmail.com>
2025-10-20 17:28:02 -0500

    animaciones y captura variables

---
+++ 02-escenario-interactivo/index.js
@@ -0,0 +1,14 @@
+//1. Captura de variables
+const jell = document.querySelectorAll(".jellyfish")
+const tablero = document.querySelector("#contador-numero")
+let counter = 0
+
+console.log(jell, tablero)
+
+//2. Funciones
+jell.forEach( item => {
+  console.log("Medusas", item)
+} )
+
+
+//3. Eventos
